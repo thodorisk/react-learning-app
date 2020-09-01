0cd9d1cb08097e8f6d87e2e0d653884e0ff40aea
thodoris <thodorisk@icloud.com>
2020-09-01 21:54:10 +0300

    Adds a stylesheet

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>i am {props.name} and i am {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed}></input>
+            <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
     )
 }
