0c27cbd4394669a39ba49e39dbc8469b87a7d8d9
thodoris <thodorisk@icloud.com>
2020-09-01 19:12:01 +0300

    Adds state and handles events

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'theo', age: 28},
+      {name: 'theo1', age: 29},
+      {name: 'theo2', age: 30},
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({ 
+      persons: [
+        {name: 'theodor', age: 35},
+        {name: 'theo1', age: 29},
+        {name: 'theo2', age: 30},
+      ]
+     })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to React</h1>
-        <Person name="theo" age="28" />
-        <Person name="theo1" age="29" />
-        <Person name="theo2" age="30">My Hobbies: ...</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: ...</Person>
       </div>
     );
   }
