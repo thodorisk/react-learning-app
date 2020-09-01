e9472928b8098c1ce7a4f735a40865a697e0afd4
thodoris <thodorisk@icloud.com>
2020-09-01 20:06:18 +0300

    Passing method reference between components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = props => {
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
-      {name: 'theo', age: 28},
-      {name: 'theo1', age: 29},
-      {name: 'theo2', age: 30},
+      { name: "theo", age: 28 },
+      { name: "theo1", age: 29 },
+      { name: "theo2", age: 30 },
     ],
-    otherState: 'some other state'
-  });
+  };
 
-  console.log(personsState)
-
-  useState('some other value'); // useState() Hook does not merge states but replaces it. Multiple useStates with different states is the way to manage state in a functional component with react hooks.
-
-  const switchNameHandler = () => {
-    setPersonsState({ 
+  switchNameHandler = () => {
+    this.setState({
       persons: [
-        {name: 'theodor', age: 35},
-        {name: 'theo1', age: 29},
-        {name: 'theo2', age: 30},
-      ]
-     })
+        { name: "theodor", age: 28 },
+        { name: "theo1", age: 29 },
+        { name: "theo2", age: 30 },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to React</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler}
+        >
+          My Hobbies: ...
+        </Person>
+      </div>
+    );
   }
+}
 
-
-  return (
-    <div className="App">
-      <h1>Welcome to React</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      />
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>
-        My Hobbies: ...
-      </Person>
-    </div>
-  );
-};
-
-export default app;
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>i am {props.name} and i am {props.age} years old!</p>
+            <p onClick={props.click}>i am {props.name} and i am {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
     )
