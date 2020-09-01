67f1f6aa1c55a3cf7ff9354b0d747a10e3021d01
thodoris <thodorisk@icloud.com>
2020-09-01 21:27:34 +0300

    Adds two way data binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
         <div>
             <p onClick={props.click}>i am {props.name} and i am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed}></input>
         </div>
     )
 }
